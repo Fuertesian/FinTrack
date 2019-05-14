234081b382b6bd976d808fb44663fbd2a8ccf713
Fuertesian <wwfuertes@gmail.com>
2019-05-14 18:54:39 -0500

    Modified router file
    Updated router to use the register methods of the newly created authentication controllers

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,13 +1,8 @@
-module.exports = (app) => {
-  // endpoints
-  app.get('/register', (req, res) => {
-    res.send({
-      message: 'registration complete' })
-  })
-  // post requests
-  app.post('/register', (req, res) => {
-    res.send({
-      message: `${req.body.email} was registered`
-    })
-  })
+const authenticationController = require('./controller/authenticationController')
+const authenticationControllerPolicy = require('./policies/authenticationControllerPolicy')
+module.exports = (app) => { // export a function that takes a parameter app
+  app.post(
+    '/register',
+    authenticationControllerPolicy.register,
+    authenticationController.register) // send a post request to the register endpoint, and execute the register function in the authetenticationController and authenticationControllerPolicy files
 }
