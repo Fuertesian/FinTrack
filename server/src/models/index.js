a1dad41ea8b3acd57cfd9070866cc5df2936dd8a
Fuertesian <wwfuertes@gmail.com>
2019-05-14 18:39:43 -0500

    Added comments to improve the clarity of code

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -15,13 +15,13 @@ fs
   .filter((file) => // creates an array with all the elements passed (in this case files) that are not index.js
     file !== 'index.js')
   .forEach((file) => { // executes this f(x) for each element in array
-    const model = sequelize.import(path.join(__dirname, file)) // import the files into sequelize to be used for model definition
-    db[model.name] = model
-  }) 
+    const model = sequelize.import(path.join(__dirname, file)) // import the files into sequelize to be used for model definition (model = tables)
+    db[model.name] = model // The returned object of the above function is the NAME of the function (sequelize.define("NAME",...)) && attach the table/model definition from the above statement to the db object
+  })
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
-module.exports = db
+db.sequelize = sequelize 
+db.Sequelize = Sequelize // allows us to access sequeilze functions with the db object
+module.exports = db // send the db object which contains the models as defined in the other files of this directory and created instance of the sequelize object
 // object constructor http://docs.sequelizejs.com/class/lib/sequelize.js~Sequelize.html#instance-constructor-constructor
 // readdirSync https://nodejs.org/api/fs.html#fs_fs_readdirsync_path_options
 // filter https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
--- server/src/models/users.js
+++ server/src/models/users.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) =>
 // export a function that takes sequlize and Datatypes as arguments
-  sequelize.define('User', {
+  sequelize.define('User', { // creates a model with the name User that is passed to the index.js file in this folder. When this is accessed by controllers, this table is defined and can be accessed or modified by user input
     email: {
       type: DataTypes.STRING,
       unique: true
